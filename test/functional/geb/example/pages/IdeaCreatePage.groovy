9982307ca8c37946182f4391b256f90895dedce5
Craig Atkinson <atk.craig@gmail.com>
2015-07-11 19:56:26 -0500

    Adding Geb test that uses slider

--- test/functional/geb/example/pages/IdeaCreatePage.groovy
+++ test/functional/geb/example/pages/IdeaCreatePage.groovy
@@ -7,13 +7,25 @@ class IdeaCreatePage extends geb.Page {
   static content = {
     titleField { $("input#title") }
     descriptionField { $("textarea#description") }
+    ratingSliderHandle { $(".ui-slider-handle") }
 
     createButton(to: IdeaShowPage) { $("input#create") }
   }
 
-  IdeaShowPage createIdea(String title, String description) {
+  IdeaShowPage createIdea(String title, String description, Integer rating = null) {
     titleField.value(title)
     descriptionField.value(description)
+
+    if (rating) {
+      // Slider is 400 pixels wide and starts at 1, so each notch above 1 is 100 pixels apart
+      Integer numPixels = (rating - 1) * 100
+
+      interact {
+        clickAndHold(ratingSliderHandle)
+        moveByOffset(numPixels, 0)
+        release()
+      }
+    }
 
     createButton.click()
 
